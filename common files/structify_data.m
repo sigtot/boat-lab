1aaa23b724d693ecc011bda9841ac945ad03faee
Odin Aleksander Severinsen <odin@severinsen.no>
2018-11-17 23:47:13 +0100

    Some work while on Ole Brum writing

--- common files/structify_data.m
+++ common files/structify_data.m
@@ -1,16 +1,16 @@
 function structified_data = structify_data(time,values_array)
 
-    [~, num_of_rows] = size(values_array);
+    [~, num_of_cols] = size(values_array);
     
-    structified_data = cell(1, num_of_rows);
+    structified_data = cell(1, num_of_cols);
     
-    for i = 1:num_of_rows
+    for i = 1:num_of_cols
        
-        structified_data{i} = new_data(time, values_array(:, i));
+        structified_data(i) = new_data(time, values_array(:, i));
         
     end
     
-    if num_of_rows == 1
+    if num_of_cols == 1
         structified_data = structified_data{1};
     end
 
--- problem 5.3/p5p3b_init.m
+++ problem 5.3/p5p3b_init.m
@@ -17,7 +17,7 @@ labels_bode_phase = new_labels(...
     'Phase plot of $H_{pd}\cdot H_{ship}$',...
     '$\angle \left(H_{pd}\cdot H_{ship}\right)$',...
     'Frequency [rad/s]',...
-    'Phase [rad]');       
+    'Phase [$^{\circ}$]');       
 
 font_size = new_font_size();
                  
@@ -28,8 +28,8 @@ font_size = new_font_size();
 K = 0.1553;  % s^-1
 T = 71.3716; % s
 T_d = T;     % So the derivative term cancels the transfer function time constant
-T_f = 10;
-K_pd = 0.707; % Phase margin 50 degrees
+T_f = 8.391;
+K_pd = 0.8406; % Phase margin 50 degrees
 
 s = tf('s');
 H_ship = (K/T)/(s*(s + 1/T));
@@ -47,25 +47,25 @@ transfer_fcn = structify_data(wout, [mag, phase]);
 % [fig_a, pl_a] = plot_nice(transfer_fcn{1}, labels_bode_amplitude, font_size, 'loglog', 'grid', 'thicklines');
 % [fig_b, pl_b] = plot_nice(transfer_fcn{2}, labels_bode_phase, font_size, 'loglog', 'grid', 'thicklines');
 
-% [sub_fig, subp] = subplot_nice(transfer_fcn', {labels_bode_amplitude; labels_bode_phase}, font_size, 'loglog', 'grid', 'thicklines');
+[sub_fig, subp] = subplot_nice(transfer_fcn', {labels_bode_amplitude; labels_bode_phase}, font_size, 'loglog', 'grid', 'thicklines');
 
 %% Task b)
 
-heading_reference = 30; % Degrees
-
-sim('p5p3b.mdl')
-
-data = new_data(heading, reference, rudder);
-title_field = strcat('Heading response to $\psi_r=30^{\circ}$, no disturbances. $K_{pd} = ', num2str(K_pd), '$');
-
-labels = new_labels(title_field,...
-                    {'$\psi_{ship}$', '$\psi_{r}$', '$\delta$'},...
-                    'Time [s]',...
-                    'Angle [$\circ$]');
-
-fig3 = plot_nice(data, labels, font_size, 'thicklines', 'grid');
- 
-filename = strcat('heading_response_no_disturbances_p5p3b K_pd_', strrep(num2str(K_pd), '.', '_dot_'));
+% heading_reference = 30; % Degrees
+% 
+% sim('p5p3b.mdl')
+% 
+% data = new_data(heading, reference, rudder);
+% title_field = strcat('Heading response to $\psi_r=30^{\circ}$, no disturbances. $K_{pd} = ', num2str(K_pd), '$');
+% 
+% labels = new_labels(title_field,...
+%                     {'$\psi_{ship}$', '$\psi_{r}$', '$\delta$'},...
+%                     'Time [s]',...
+%                     'Angle [$\circ$]');
+% 
+% fig3 = plot_nice(data, labels, font_size, 'thicklines', 'grid');
+%  
+% filename = strcat('heading_response_no_disturbances_p5p3b K_pd_', strrep(num2str(K_pd), '.', '_dot_'));
 
 % plot2pdf(PLOT_PATH, filename, fig3);
 
